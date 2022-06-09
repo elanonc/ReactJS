b231b7936f8933da5b53b8e0891cfa0aa3f727c1
Elano N. Caitano <nunes.elano14@gmail.com>
2022-06-09 18:59:31 -0300

    Exercicio UseEffect

--- exercicio-useeffect/src/App.jsx
+++ exercicio-useeffect/src/App.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import './App.css';
-// import Produto from './components/Produto';
+import Produto from './components/Produto';
 
 function App() {
-  const [produto, setProdutos] = React.useState(null);
+  const [produto, setProduto] = React.useState(null);
+
+  React.useEffect(() => {
+    const produtoLocal = window.localStorage.getItem('produto');
+    if(produtoLocal != null) setProduto(produtoLocal);
+  }, []);
+
+  React.useEffect(() => {
+    if(produto != null) window.localStorage.setItem('produto', produto);
+  }, [produto]);
 
   function handleClick ({target}) {
-    setProdutos(target.innerText);  
+    setProduto(target.innerText);  
   }
 
   return (
     <div className="App">
-      <h1>PREFERÊNCIA</h1>
+      <h1>Preferência: {produto}</h1>
       <button onClick={handleClick} >
         notebook
       </button> 
       <button onClick={handleClick}> 
         smartphone
       </button>   
+      <Produto produto={produto} /> 
     </div>
   )
 }
